528ac2111f3f4ad7cd9e1f4564def2199b141fa7
Vanessa Farias <vr.farias1992@gmail.com>
2022-09-08 16:08:00 -0300

    exercicios sobre Condicionais II - JS

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,25 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+//nome
+const nome = prompt("Diga seu nome")
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome);
+
+//idade
+const idade = prompt("Diga sua idade")
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista");
+
